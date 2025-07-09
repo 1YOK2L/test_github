fe1af42648376d62208c4e96be48c0955b8ea45c
1YOK2L <watpat.tanti@gmail.com>
2025-07-09 16:23:01 +0700

    test django

---
+++ Programming/settings.py
@@ -0,0 +1,17 @@
+# settings.py
+DEBUG = True
+SECRET_KEY = 'MySecretKey'
+ROOT_URLCONF = 'hello_world'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
