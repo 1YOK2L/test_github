44cf12f666d62804e5f99627a2d3fb5b5b9ecf39
1YOK2L <watpat.tanti@gmail.com>
2025-07-09 16:29:17 +0700

    django

--- Programming/manage.py
+++ Programming/manage.py
@@ -4,5 +4,5 @@ from django.core.management import execute_from_command_line
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_world.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Programming.settings')
     execute_from_command_line(sys.argv)
