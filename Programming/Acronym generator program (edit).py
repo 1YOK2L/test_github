3881c1a07e488195968b039b7e09507e2ef179ec
1YOK2L <watpat.tanti@gmail.com>
2025-07-09 11:20:14 +0700

    acronym

--- Programming/Acronym generator program (edit).py
+++ Programming/Acronym generator program (edit).py
@@ -3,11 +3,11 @@ from functools import reduce
 
 def measure_time(func):
     def wrapper(x):
+        global elapsed_time
         start_time = time.time()
         result = func(x)
         end_time = time.time()
         elapsed_time = end_time - start_time
-        print(f"Operation time: {elapsed_time:.10f} seconds")
         return result
     return wrapper
 
@@ -16,13 +16,4 @@ def acronym(x):
     x = x.split()
     return reduce(lambda acc, word: acc + word[0].upper(), x, "")
 print(acronym("portable network graphics"))
-
-# # Get the input
-# string = input("Enter a text: ").split()
-
-# # Call acronym and get the result along with operation time
-# result, elapsed_time = acronym(string)
-
-# # Print results in the desired format
-# print(f"Acronym: {result}")
-# print(f"Operation time: {elapsed_time:.10f} seconds")
+print(f"Operation time: {elapsed_time:.10f} seconds")
